d5c4f2d3f9d0d8fa1325b411ff805d1a3d10bc99
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2024-12-06 20:20:05 +0530

    System Updated

--- Server/Routes/methods.js
+++ Server/Routes/methods.js
@@ -31,25 +31,5 @@ router.get('/admin/:id', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
-router.put('/admin/:id', async (req, res) => {
-    try {
-        const updatedPlayer = await Players.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!updatedPlayer) return res.status(404).json({ error: 'Players not found' });
-        res.json(updatedPlayer);  // Ensure this returns the updated player data
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-});
-
-
-router.delete('/admin/:id', async (req, res) => {
-    try {
-        const deletedPlayer = await Players.findByIdAndDelete(req.params.id);
-        if (!deletedPlayer) return res.status(404).json({ error: 'Players not found' });
-        res.status(204).send();
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
 
 module.exports = router;
